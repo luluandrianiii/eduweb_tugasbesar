6aac43fa68a31bd7d9494773b8b072f9743ae090
luluandrianiii <luluandriani980@gmail.com>
2025-06-10 14:12:33 +0700

    initial commit

---
+++ admin/add_playlist.php
@@ -0,0 +1,54 @@
+<?php
+    include '../components/connect.php';
+
+    if(isset($_COOKIE['tutor_id'])){
+        $tutor_id = $_COOKIE['tutor_id'];
+    }else{
+        $tutor_id = '';
+        header('location: login.php');
+    }
+
+    if (isset($_POST['submit'])){
+        $id = unique_id();
+        $title = $_POST['title'];
+        $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+    }
+
+?>
+<style type="text/css">
+    <?php include '../css/admin_style.css'; ?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Dashboard</title>
+</head>
+<body>
+    <?php include '../components/admin_header.php'?>
+    <section class="playlist-form">
+        <h1 class="heading">Buat Playlist</h1>
+                  
+        <form action="" method ="post" enctype="multipart/form-data">
+            <p>Playlist Status <span>*</span></p>
+            <select name="status" class="box">
+                <option value="" selected disabled>--Pilih Status</option>
+                <option value="active">Aktif</option>
+                <option value="deactive">Non-Aktif</option>
+            </select>
+            <p>Judul Playlist <span>*</span></p>
+            <input type="text" name="title" maxlength="100" required placeholder="Masukkan Judul Playlist" class="box">
+            <p>Deskripsi Playlist <span>*</span></p>
+            <textarea name="description" class="box" placeholder="Tulis Deskripsi" maxlength="1000" cols="30" rows="10"></textarea>
+            <p>Thumbnail Playlist <span>*</span></p>
+            <input type="file" name="image" accept="image/*" required class="box">
+            <input type="submit" name="submit" value="buat playlist" class="btn">
+        </form>
+    </section>
+    <?php include '../components/footer.php'?>
+    <script src="../components/admin_script.js" defer></script>
+</body>
+</html>
